4729c154506a37fdba0d83e870323a93ae995c13
derker <893309018@qq.com>
2018-05-03 21:13:58 +0800

    clean

--- cms/src/main/java/com/xtuwebteam/dao/mapper/NewsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xtuwebteam.dao.mapper;
-
-import com.xtuwebteam.dao.base.BaseMapper;
-import com.xtuwebteam.domain.entity.News;
-
-public interface NewsMapper extends BaseMapper<News> {
-}
